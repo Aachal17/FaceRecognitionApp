a939ed111087bbfaaee4c10a9ca74b0d3dd16b0d
Aachal Shah <aachalshah.garg@gmail.com>
2025-03-24 21:09:29 +0530

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace MultiFaceRec
+{
+    partial class FrmPrincipal
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBoxFrameGrabber = new Emgu.CV.UI.ImageBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxMatches = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button(); // New Search button
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMatches)).BeginInit();
+            this.SuspendLayout();
+
+            // button2
+            this.button2.Location = new System.Drawing.Point(87, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2. Add face";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+
+            // textBox1
+            this.textBox1.Location = new System.Drawing.Point(67, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 20);
+            this.textBox1.TabIndex = 7;
+
+            // groupBox1
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.imageBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(342, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 242);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.Text = "Training: ";
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Name: ";
+
+            // imageBox1
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(11, 18);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(163, 134);
+            this.imageBox1.TabIndex = 5;
+
+            // groupBox2
+            this.groupBox2.Controls.Add(this.button3); // Add button3 to groupBox2
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(532, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(209, 242);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.Text = "Results: ";
+
+            // label5
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(9, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 15);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Persons present in the scene:";
+
+            // label4
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(9, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 19);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Nobody";
+
+            // label3
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(163, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "0";
+
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(10, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 15);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Number of faces detected: ";
+
+            // button1
+            this.button1.Location = new System.Drawing.Point(84, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1. Detect and recognize";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+
+            // button3 (New Search button)
+            this.button3.Location = new System.Drawing.Point(84, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 20);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "3. Search Matches";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+
+            // imageBoxFrameGrabber
+            this.imageBoxFrameGrabber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBoxFrameGrabber.Location = new System.Drawing.Point(12, 12);
+            this.imageBoxFrameGrabber.Name = "imageBoxFrameGrabber";
+            this.imageBoxFrameGrabber.Size = new System.Drawing.Size(320, 240);
+            this.imageBoxFrameGrabber.TabIndex = 4;
+
+            // groupBox3
+            this.groupBox3.Controls.Add(this.pictureBoxMatches);
+            this.groupBox3.Location = new System.Drawing.Point(12, 258);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(729, 150);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.Text = "Matching Images: ";
+
+            // pictureBoxMatches
+            this.pictureBoxMatches.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxMatches.Location = new System.Drawing.Point(6, 19);
+            this.pictureBoxMatches.Name = "pictureBoxMatches";
+            this.pictureBoxMatches.Size = new System.Drawing.Size(717, 125);
+            this.pictureBoxMatches.TabIndex = 0;
+
+            // FrmPrincipal
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 414); // Adjusted height for new groupBox
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.imageBoxFrameGrabber);
+            this.Name = "FrmPrincipal";
+            this.Text = "Camera Input & Face Detection";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMatches)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBoxFrameGrabber;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBoxMatches;
+        private System.Windows.Forms.Button button3; // New Search button
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.IO;
+
+namespace MultiFaceRec
+{
+    public partial class FrmPrincipal : Form
+    {
+        Image<Bgr, Byte> currentFrame;
+        Capture grabber;
+        HaarCascade face;
+        MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 0.5d, 0.5d);
+        Image<Gray, byte> result, TrainedFace = null;
+        Image<Gray, byte> gray = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> NamePersons = new List<string>();
+        int ContTrain, NumLabels, t;
+        string name, names = null;
+        string databasePath = Path.Combine(Application.StartupPath, "FaceDatabase");
+        string photoFolderPath = @"C:\Users\Aachal\Source\Repos\Face-Detection-Clone\FaceRecProOV\EventImages";
+        Image<Gray, byte> lastDetectedFace = null;
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            face = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                string Labelsinfo = File.ReadAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt");
+                string[] Labels = Labelsinfo.Split('%');
+                NumLabels = Convert.ToInt16(Labels[0]);
+                ContTrain = NumLabels;
+                for (int tf = 1; tf < NumLabels + 1; tf++)
+                {
+                    string LoadFaces = "face" + tf + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/TrainedFaces/" + LoadFaces));
+                    labels.Add(Labels[tf]);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No trained faces in database. Add a face to proceed.", "Load Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            if (!Directory.Exists(databasePath)) Directory.CreateDirectory(databasePath);
+            if (!Directory.Exists(photoFolderPath)) Directory.CreateDirectory(photoFolderPath);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            grabber = new Capture();
+            grabber.QueryFrame();
+            Application.Idle += new EventHandler(FrameGrabber);
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Enter a name to add face.");
+                return;
+            }
+
+            ContTrain++;
+            gray = grabber.QueryGrayFrame().Resize(320, 240, INTER.CV_INTER_CUBIC);
+            MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(face, 1.2, 10, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+
+            if (facesDetected[0].Length > 0)
+            {
+                TrainedFace = currentFrame.Copy(facesDetected[0][0].rect).Convert<Gray, byte>().Resize(100, 100, INTER.CV_INTER_CUBIC);
+                trainingImages.Add(TrainedFace);
+                labels.Add(textBox1.Text);
+                imageBox1.Image = TrainedFace;
+
+                string fileName = Path.Combine(databasePath, $"{textBox1.Text}_{DateTime.Now.Ticks}.bmp");
+                TrainedFace.Save(fileName);
+
+                File.WriteAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", trainingImages.Count + "%");
+                for (int i = 1; i <= trainingImages.Count; i++)
+                {
+                    trainingImages[i - 1].Save(Application.StartupPath + "/TrainedFaces/face" + i + ".bmp");
+                    File.AppendAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", labels[i - 1] + "%");
+                }
+
+                MessageBox.Show(textBox1.Text + "'s face added.", "Training OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No face detected. Try again.", "Training Fail", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        void FrameGrabber(object sender, EventArgs e)
+        {
+            label3.Text = "0";
+            NamePersons.Clear();
+            currentFrame = grabber.QueryFrame().Resize(320, 240, INTER.CV_INTER_CUBIC);
+            gray = currentFrame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(face, 1.2, 10, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+
+            foreach (MCvAvgComp f in facesDetected[0])
+            {
+                t++;
+                result = currentFrame.Copy(f.rect).Convert<Gray, byte>().Resize(100, 100, INTER.CV_INTER_CUBIC);
+                currentFrame.Draw(f.rect, new Bgr(Color.Green), 2);
+
+                if (trainingImages.Count > 0)
+                {
+                    MCvTermCriteria termCrit = new MCvTermCriteria(ContTrain, 0.001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 5000, ref termCrit);
+                    name = recognizer.Recognize(result);
+                    currentFrame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+                    lastDetectedFace = result.Clone();
+                }
+
+                NamePersons.Add(name);
+            }
+
+            label3.Text = facesDetected[0].Length.ToString();
+            names = string.Join(", ", NamePersons);
+            imageBoxFrameGrabber.Image = currentFrame;
+            label4.Text = names;
+            names = "";
+            NamePersons.Clear();
+            t = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (lastDetectedFace == null)
+            {
+                MessageBox.Show("No face detected. Scan a face first.", "Search Failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (trainingImages.Count == 0)
+            {
+                MessageBox.Show("No trained faces available.", "Search Failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            MCvTermCriteria termCrit = new MCvTermCriteria(ContTrain, 0.001);
+            EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 5000, ref termCrit);
+            string recognizedName = recognizer.Recognize(lastDetectedFace);
+
+            if (!string.IsNullOrEmpty(recognizedName))
+            {
+                SearchAndDisplayMatches(recognizedName);
+            }
+            else
+            {
+                pictureBoxMatches.Image = null;
+                MessageBox.Show("No match found.", "Search Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void SearchAndDisplayMatches(string name)
+        {
+            string[] photoFiles = Directory.GetFiles(photoFolderPath, "*.bmp");
+            List<Image<Bgr, byte>> matchingImages = new List<Image<Bgr, byte>>();
+
+            foreach (string file in photoFiles)
+            {
+                try
+                {
+                    using (Image<Bgr, byte> img = new Image<Bgr, byte>(file))
+                    {
+                        Image<Gray, byte> grayImg = img.Convert<Gray, byte>();
+                        MCvAvgComp[][] facesDetected = grayImg.DetectHaarCascade(face, 1.1, 5, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+
+                        foreach (MCvAvgComp f in facesDetected[0])
+                        {
+                            Image<Gray, byte> faceImg = grayImg.Copy(f.rect).Resize(100, 100, INTER.CV_INTER_CUBIC);
+                            MCvTermCriteria termCrit = new MCvTermCriteria(ContTrain, 0.001);
+                            EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 5000, ref termCrit);
+                            if (recognizer.Recognize(faceImg) == name)
+                            {
+                                matchingImages.Add(img.Clone()); // Clone to keep image in memory
+                                break;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error processing {Path.GetFileName(file)}: {ex.Message}", "Image Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+
+            if (matchingImages.Count > 0)
+            {
+                try
+                {
+                    int maxWidth = 100; // Fixed width for thumbnails
+                    int maxHeight = 100; // Fixed height for thumbnails
+                    Bitmap combinedImage = new Bitmap(maxWidth * matchingImages.Count, maxHeight);
+
+                    using (Graphics g = Graphics.FromImage(combinedImage))
+                    {
+                        for (int i = 0; i < matchingImages.Count; i++)
+                        {
+                            using (Bitmap thumbnail = new Bitmap(matchingImages[i].ToBitmap(), maxWidth, maxHeight))
+                            {
+                                g.DrawImage(thumbnail, i * maxWidth, 0);
+                            }
+                            matchingImages[i].Dispose();
+                        }
+                    }
+
+                    pictureBoxMatches.Image?.Dispose();
+                    pictureBoxMatches.Image = combinedImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error displaying images: {ex.Message}", "Display Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                pictureBoxMatches.Image?.Dispose();
+                pictureBoxMatches.Image = null;
+                MessageBox.Show("No matching images found in EventImages.", "Search Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
